9662a33ea02332791e94fabb3c35572c9d8a7d82
draco <2069629199@qq.com>
2020-02-29 11:39:32 +0800

    静态链表初始化功能完成

---
+++ StaticLinkedList/main.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define maxSize 6
+
+//--------创建静态链表结点结构--------
+typedef struct
+{
+    int elem;
+    int cur;
+}component;
+
+//--------函数声明--------
+void reserveArr(component* array);     //生成并连接备用链表
+int mallocArr(component* array);      //分配一个数据节点
+int initArr();      //初始化链表
+void display(component* array,int body);  //打印链表
+
+int main()
+{
+    component array[maxSize];
+    int body=initArr(array);
+    display(array,body);
+    system("pause");
+}
+
+//--------函数定义--------
+void reserveArr(component* array)
+{
+    for(int i=0;i<maxSize;i++)
+    {
+        array[i].elem=-1;
+        array[i].cur=i+1;
+    }
+    array[maxSize-1].cur=0;
+}
+
+int mallocArr(component* array)
+{
+    int i=array[0].cur;
+    if(array[0].cur)
+    {
+        array[0].cur=array[i].cur;
+    }
+    return i;
+}
+
+int initArr(component* array)
+{
+    reserveArr(array);
+    int body=mallocArr(array);
+    int tempBody=body;
+    for(int i=1;i<4;i++)
+    {
+        int a=mallocArr(array);
+        array[a].elem=i;
+        array[a].cur=0;
+        array[tempBody].cur=a;
+        tempBody=a;
+    }
+    return body;
+}
+
+void display(component* array,int body)
+{
+    int tempBody=body;
+    tempBody=array[tempBody].cur;
+    while(array[tempBody].cur)
+    {
+        printf("%d--%d  ",array[tempBody].elem,array[tempBody].cur);
+        tempBody=array[tempBody].cur;
+    }   
+    printf("%d--%d  \n",array[tempBody].elem,array[tempBody].cur);
+}
